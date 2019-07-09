2759896bacd6cfed3f8507ae881bf351e6807849
Lâm Thành Lộc <wankygame@gmail.com>
2019-07-09 17:15:32 +0700

    Add effect

---
+++ precache-manifest.bc079e4e821f36e9eb9162fef9000f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce694086a44901760d69afe855d0f73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b407a9ae823fba16cd6",
+    "url": "/static/css/main.2fce3b71.chunk.css"
+  },
+  {
+    "revision": "e7f78e482e6a03cf7241",
+    "url": "/static/js/2.12025348.chunk.js"
+  },
+  {
+    "revision": "6b407a9ae823fba16cd6",
+    "url": "/static/js/main.713b0ecd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d9d626e66736c9d3dbc4ab55b7e8b9d5",
+    "url": "/static/media/main-photo.d9d626e6.jpg"
+  }
+]);
